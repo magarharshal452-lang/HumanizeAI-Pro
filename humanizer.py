0b7cb23abea44566e10b9ebfeb3f91a9c7a9dd34
magarharshal452-lang <magarharshal452@gmail.com>
2026-06-13 18:43:35 +0530

    Update humanizer.py

--- humanizer.py
+++ humanizer.py
@@ -2,21 +2,19 @@ from transformers import pipeline
 from prompts import PROMPTS
 
 generator = pipeline(
-    "text2text-generation",
-    model="google/flan-t5-large"
+    task="text2text-generation",
+    model="google/flan-t5-base"
 )
 
 def humanize(text, mode):
 
-    prompt = PROMPTS[mode].format(
-        text=text
-    )
+    prompt = PROMPTS[mode].format(text=text)
 
     result = generator(
         prompt,
         max_length=512,
         do_sample=True,
-        temperature=0.8
+        temperature=0.7
     )
 
     return result[0]["generated_text"]
